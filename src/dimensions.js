5b40d95f15ee55118dc75e6afe4476db35437884
Creeper Panda T Rex <paintorange-nelson@outlook.com>
2023-06-15 17:49:01 -0700

    Added plugins red border

--- src/dimensions.js
+++ src/dimensions.js
@@ -4,11 +4,35 @@ import { getDimensions } from './api'
 (async function () {
     const data = await getDimensions();
 
+    const chartAreaBorder = {
+        id: 'chartAreaBorder',
+
+        beforeDraw(chart, args, options) {
+            const { ctx, chartArea: { left, top, width, height } } = chart;
+
+            ctx.save();
+            ctx.strokeStyle = options.borderColor;
+            ctx.lineWidth = options.borderWidth;
+            ctx.setLineDash(options.borderDash || []);
+            ctx.lineDashOffset = options.borderDashOffset;
+            ctx.strokeRect(left, top, width, height);
+            ctx.restore();
+        }
+    };
     new Chart(
         document.getElementById('dimensions'),
         {
             type: 'bubble',
+            plugins: [ chartAreaBorder ],
             options: {
+                plugins: {
+                    chartAreaBorder: {
+                        borderColor: 'red',
+                        borderWidth: 2,
+                        borderDash: [5, 5],
+                        borderDashOffset: 2,
+                    }
+                },
                 aspectRatio: 1,
                 scales: {
                     x: {
