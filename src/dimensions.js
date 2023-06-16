93cbf4162ea1e6bd274415ab17d6fb4caf2953bd
Creeper Panda T Rex <paintorange-nelson@outlook.com>
2023-06-15 17:22:20 -0700

    Bubble char Multiple datasets

--- src/dimensions.js
+++ src/dimensions.js
@@ -11,24 +11,53 @@ import { getDimensions } from './api'
             options: {
                 aspectRatio: 1,
                 scales: {
-                    x: {max: 500, ticks: {
-                        callback: value => `${value / 100} m`
-                    }},
-                    y: {max: 500}
+                    x: {
+                        max: 500, ticks: {
+                            callback: value => `${value / 100} m`
+                        }
+                    },
+                    y: { max: 500 }
                 }
             },
             data: {
                 labels: data.map(x => x.year),
+
+
                 datasets: [
                     {
-                        label: 'Dimensions',
-                        data: data.map(row => ({
-                            x: row.width,
-                            y: row.height,
-                            r: row.count
-                        }))
+                        label: 'width = height',
+                        data: data
+                            .filter(row => row.width === row.height)
+                            .map(row => ({
+                                x: row.width,
+                                y: row.height,
+                                r: row.count
+                            }))
+                    },
+                    {
+                        label: 'width > height',
+                        data: data
+                            .filter(row => row.width > row.height)
+                            .map(row => ({
+                                x: row.width,
+                                y: row.height,
+                                r: row.count
+                            }))
+                    },
+                    {
+                        label: 'width < height',
+                        data: data
+                            .filter(row => row.width < row.height)
+                            .map(row => ({
+                                x: row.width,
+                                y: row.height,
+                                r: row.count
+                            }))
                     }
                 ]
+
+
+
             }
         }
     );
